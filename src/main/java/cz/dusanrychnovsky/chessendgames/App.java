f6e6b4792cc282049ad81cfed24e4bfa28580fc5
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2019-11-30 18:09:19 +0100

    Init empty project.

---
+++ src/main/java/cz/dusanrychnovsky/chessendgames/App.java
@@ -0,0 +1,9 @@
+package cz.dusanrychnovsky.chessendgames;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cz/dusanrychnovsky/chessendgames/AppTest.java
@@ -0,0 +1,10 @@
+package cz.dusanrychnovsky.chessendgames;
+
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void main() {
+    App.main(new String[0]);
+  }
+}
