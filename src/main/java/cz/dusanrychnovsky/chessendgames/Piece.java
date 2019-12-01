d2132f6d878172da2c99a61bdb38c3f818bcc5e0
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2019-12-01 12:10:50 +0100

    Implement Piece#parse.

--- src/main/java/cz/dusanrychnovsky/chessendgames/Piece.java
+++ src/main/java/cz/dusanrychnovsky/chessendgames/Piece.java
@@ -1,5 +1,11 @@
 package cz.dusanrychnovsky.chessendgames;
 
+import static cz.dusanrychnovsky.chessendgames.StringExtensions.capitalize;
+
 public enum Piece {
-  King, Rook
+  King, Rook;
+
+  public static Piece parse(String value) {
+    return valueOf(capitalize(value));
+  }
 }
---
+++ src/test/java/cz/dusanrychnovsky/chessendgames/PieceTest.java
@@ -0,0 +1,31 @@
+package cz.dusanrychnovsky.chessendgames;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import static cz.dusanrychnovsky.chessendgames.Piece.*;
+
+public class PieceTest {
+
+  // ==========================================================================
+  // Parsing
+  // ==========================================================================
+
+  @Test
+  public void parse_supportsLowerCase() {
+    assertEquals(King, parse("king"));
+    assertEquals(Rook, parse("rook"));
+  }
+
+  @Test
+  public void parse_supportsUpperCase() {
+    assertEquals(King, parse("KING"));
+    assertEquals(Rook, parse("ROOK"));
+  }
+
+  @Test
+  public void parse_supportsCamelCase() {
+    assertEquals(King, parse("King"));
+    assertEquals(Rook, parse("Rook"));
+  }
+}
