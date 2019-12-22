d11f6dfdd31c440fe4a3fd6075272f598d613161
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2019-12-22 21:49:39 +0100

    Implement Position#column and #row.

--- src/main/java/cz/dusanrychnovsky/chessendgames/Position.java
+++ src/main/java/cz/dusanrychnovsky/chessendgames/Position.java
@@ -1,11 +1,29 @@
 package cz.dusanrychnovsky.chessendgames;
 
+import static cz.dusanrychnovsky.chessendgames.Column.*;
+import static cz.dusanrychnovsky.chessendgames.Row.*;
 import static cz.dusanrychnovsky.chessendgames.StringExtensions.capitalize;
 
 public enum Position {
-  F5, E6, D4, D3;
+  F5(CF, R5), E6(CE, R6), D4(CD, R4), D3(CD, R3);
+
+  private final Column column;
+  private final Row row;
+
+  Position(Column column, Row row) {
+    this.column = column;
+    this.row = row;
+  }
 
   public static Position parse(String value) {
     return valueOf(capitalize(value));
+  }
+
+  public Column column() {
+    return this.column;
+  }
+
+  public Row row() {
+    return this.row;
   }
 }
--- src/test/java/cz/dusanrychnovsky/chessendgames/PositionTest.java
+++ src/test/java/cz/dusanrychnovsky/chessendgames/PositionTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import static cz.dusanrychnovsky.chessendgames.Position.*;
+import static cz.dusanrychnovsky.chessendgames.Column.CD;
+import static cz.dusanrychnovsky.chessendgames.Row.R3;
 
 public class PositionTest {
 
@@ -19,5 +21,19 @@ public class PositionTest {
   @Test
   public void parse_supportsUpperCase() {
     assertEquals(D3, parse("D3"));
+  }
+
+  // ==========================================================================
+  // Destructuring
+  // ==========================================================================
+
+  @Test
+  public void column() {
+    assertEquals(CD, D3.column());
+  }
+
+  @Test
+  public void row() {
+    assertEquals(R3, D3.row());
   }
 }
