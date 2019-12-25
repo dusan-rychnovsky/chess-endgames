5d88dd656620a4b632a1364d5636258e014699ad
Dusan Rychnovsky <durychno@microsoft.com>
2019-12-25 21:50:45 +0100

    Refactoring.

--- src/main/java/cz/dusanrychnovsky/chessendgames/Piece.java
+++ src/main/java/cz/dusanrychnovsky/chessendgames/Piece.java
@@ -1,8 +1,13 @@
 package cz.dusanrychnovsky.chessendgames;
 
+import lombok.Value;
+import lombok.experimental.Accessors;
+
 import static cz.dusanrychnovsky.chessendgames.Color.*;
 import static cz.dusanrychnovsky.chessendgames.PieceType.*;
 
+@Value
+@Accessors(fluent = true)
 public class Piece {
 
   public static final Piece WhiteKing = new Piece(White, King);
@@ -10,36 +15,6 @@ public class Piece {
   public static final Piece BlackKing = new Piece(Black, King);
   public static final Piece BlackRook = new Piece(Black, Rook);
 
-  private final Color color;
-  private final PieceType type;
-
-  public Piece(Color color, PieceType type) {
-    this.color = color;
-    this.type = type;
-  }
-
-  public Color color() {
-    return this.color;
-  }
-
-  public PieceType type() {
-    return this.type;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (!(obj instanceof Piece)) {
-      return false;
-    }
-
-    var other = (Piece) obj;
-    return
-      this.color == other.color &&
-      this.type == other.type;
-  }
-
-  @Override
-  public int hashCode() {
-    return this.color.hashCode() + this.type.hashCode();
-  }
+  Color color;
+  PieceType type;
 }
