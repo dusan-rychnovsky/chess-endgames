04f0366d6eb41785a51d7fd9333326068c814406
Dusan Rychnovsky <durychno@microsoft.com>
2019-12-25 21:29:31 +0100

    Refactoring.

--- src/main/java/cz/dusanrychnovsky/chessendgames/Piece.java
+++ src/main/java/cz/dusanrychnovsky/chessendgames/Piece.java
@@ -1,6 +1,14 @@
 package cz.dusanrychnovsky.chessendgames;
 
+import static cz.dusanrychnovsky.chessendgames.Color.*;
+import static cz.dusanrychnovsky.chessendgames.PieceType.*;
+
 public class Piece {
+
+  public static final Piece WhiteKing = new Piece(White, King);
+  public static final Piece WhiteRook = new Piece(White, Rook);
+  public static final Piece BlackKing = new Piece(Black, King);
+  public static final Piece BlackRook = new Piece(Black, Rook);
 
   private final Color color;
   private final PieceType type;
--- src/test/java/cz/dusanrychnovsky/chessendgames/BoardTest.java
+++ src/test/java/cz/dusanrychnovsky/chessendgames/BoardTest.java
@@ -1,15 +1,14 @@
 package cz.dusanrychnovsky.chessendgames;
 
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Optional;
-
 import static org.junit.Assert.assertEquals;
 
+import static cz.dusanrychnovsky.chessendgames.Piece.*;
 import static cz.dusanrychnovsky.chessendgames.Color.*;
 import static cz.dusanrychnovsky.chessendgames.PieceType.*;
 import static cz.dusanrychnovsky.chessendgames.Position.*;
+
+import java.util.Optional;
 
 public class BoardTest {
 
@@ -31,26 +30,23 @@ public class BoardTest {
 
   @Test
   public void builder_positionsPiecesOnBoard() {
-    Piece whiteKing = new Piece(White, King);
-    Piece blackKing = new Piece(Black, King);
-    Piece blackRook = new Piece(Black, Rook);
     Board board = Board.builder()
-      .add(whiteKing, D3)
-      .add(blackKing, F5)
-      .add(blackRook, E6)
+      .add(WhiteKing, D3)
+      .add(BlackKing, F5)
+      .add(BlackRook, E6)
       .build();
 
-    assertEquals(whiteKing, board.atPosition(D3).get());
-    assertEquals(blackKing, board.atPosition(F5).get());
-    assertEquals(blackRook, board.atPosition(E6).get());
+    assertEquals(WhiteKing, board.atPosition(D3).get());
+    assertEquals(BlackKing, board.atPosition(F5).get());
+    assertEquals(BlackRook, board.atPosition(E6).get());
     assertEquals(Optional.empty(), board.atPosition(D4));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void builder_duplicatePosition_fails() {
     Board.builder()
-      .add(new Piece(Black, King), D3)
-      .add(new Piece(White, King), D3);
+      .add(BlackKing, D3)
+      .add(WhiteKing, D3);
   }
 
   // ==========================================================================
@@ -76,13 +72,10 @@ public class BoardTest {
 
   @Test
   public void print_boardWithPieces() {
-    Piece whiteKing = new Piece(White, King);
-    Piece blackKing = new Piece(Black, King);
-    Piece blackRook = new Piece(Black, Rook);
     Board board = Board.builder()
-      .add(whiteKing, D3)
-      .add(blackKing, F5)
-      .add(blackRook, E6)
+      .add(WhiteKing, D3)
+      .add(BlackKing, F5)
+      .add(BlackRook, E6)
       .build();
     assertEquals(
       "8 | . . . . . . . .\n" +
