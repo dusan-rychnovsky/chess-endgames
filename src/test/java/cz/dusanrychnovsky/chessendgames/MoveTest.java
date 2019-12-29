847105d1debec9fa0309ae5dccc44f33b7d89210
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2019-12-29 20:20:43 +0100

    Implement Move#equals and #hashCode.

--- src/test/java/cz/dusanrychnovsky/chessendgames/MoveTest.java
+++ src/test/java/cz/dusanrychnovsky/chessendgames/MoveTest.java
@@ -2,6 +2,7 @@ package cz.dusanrychnovsky.chessendgames;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import static cz.dusanrychnovsky.chessendgames.Position.*;
 
@@ -25,5 +26,27 @@ public class MoveTest {
   public void parse_supportsMixedCase() {
     assertEquals(new Move(D3, E5), Move.parse("d3 E5"));
     assertEquals(new Move(D3, E5), Move.parse("D3 e5"));
+  }
+
+  // ==========================================================================
+  // Equals & HashCode
+  // ==========================================================================
+
+  @Test
+  public void sameMoves_areEqual() {
+    assertEquals(new Move(D3, E5), new Move(D3, E5));
+    assertEquals(new Move(D3, E5).hashCode(), new Move(D3, E5).hashCode());
+  }
+
+  @Test
+  public void differentFrom_areNotEqual() {
+    assertNotEquals(new Move(D3, E5), new Move(D2, E5));
+    assertNotEquals(new Move(D3, E5).hashCode(), new Move(D2, E5).hashCode());
+  }
+
+  @Test
+  public void differentTo_areNotEqual() {
+    assertNotEquals(new Move(D3, E5), new Move(D3, E4));
+    assertNotEquals(new Move(D3, E5).hashCode(), new Move(D3, E4).hashCode());
   }
 }
