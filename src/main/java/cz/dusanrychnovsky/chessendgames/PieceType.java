0a751ce82cd81490ce3386dee53576de88978a74
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2019-12-30 10:32:31 +0100

    Implement Rook#moves.

--- src/main/java/cz/dusanrychnovsky/chessendgames/PieceType.java
+++ src/main/java/cz/dusanrychnovsky/chessendgames/PieceType.java
@@ -2,10 +2,43 @@ package cz.dusanrychnovsky.chessendgames;
 
 import static cz.dusanrychnovsky.chessendgames.StringExtensions.capitalize;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static java.util.Arrays.stream;
+import static java.util.stream.Stream.concat;
+
 public enum PieceType {
-  King, Rook;
+
+  King {
+    @Override
+    public Set<Move> moves(Position from) {
+      return null;
+    }
+  },
+
+  Rook {
+
+    @Override
+    public Set<Move> moves(Position from) {
+      return concat(column(from), row(from))
+        .filter(pos -> pos != from)
+        .map(pos -> new Move(from, pos))
+        .collect(Collectors.toSet());
+    }
+
+    private Stream<Position> column(Position from) {
+      return stream(Position.values()).filter(pos -> pos.row() == from.row());
+    }
+
+    private Stream<Position> row(Position from) {
+      return stream(Position.values()).filter(pos -> pos.column() == from.column());
+    }
+  };
 
   public static PieceType parse(String value) {
     return valueOf(capitalize(value));
   }
+
+  public abstract Set<Move> moves(Position from);
 }
--- src/test/java/cz/dusanrychnovsky/chessendgames/PieceTypeTest.java
+++ src/test/java/cz/dusanrychnovsky/chessendgames/PieceTypeTest.java
@@ -2,8 +2,12 @@ package cz.dusanrychnovsky.chessendgames;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import static cz.dusanrychnovsky.chessendgames.Position.*;
 import static cz.dusanrychnovsky.chessendgames.PieceType.*;
+
+import java.util.Set;
 
 public class PieceTypeTest {
 
@@ -13,19 +17,51 @@ public class PieceTypeTest {
 
   @Test
   public void parse_supportsLowerCase() {
-    assertEquals(King, parse("king"));
-    assertEquals(Rook, parse("rook"));
+    assertEquals(King, PieceType.parse("king"));
+    assertEquals(Rook, PieceType.parse("rook"));
   }
 
   @Test
   public void parse_supportsUpperCase() {
-    assertEquals(King, parse("KING"));
-    assertEquals(Rook, parse("ROOK"));
+    assertEquals(King, PieceType.parse("KING"));
+    assertEquals(Rook, PieceType.parse("ROOK"));
   }
 
   @Test
   public void parse_supportsCamelCase() {
-    assertEquals(King, parse("King"));
-    assertEquals(Rook, parse("Rook"));
+    assertEquals(King, PieceType.parse("King"));
+    assertEquals(Rook, PieceType.parse("Rook"));
+  }
+
+  // ==========================================================================
+  // Rook Moving
+  // ==========================================================================
+
+  @Test
+  public void rook_moves_returnsRowAndColumn() {
+    assertSetEquals(
+      Set.of(
+        new Move(D3, D1),
+        new Move(D3, D2),
+        new Move(D3, D4),
+        new Move(D3, D5),
+        new Move(D3, D6),
+        new Move(D3, D7),
+        new Move(D3, D8),
+        new Move(D3, A3),
+        new Move(D3, B3),
+        new Move(D3, C3),
+        new Move(D3, E3),
+        new Move(D3, F3),
+        new Move(D3, G3),
+        new Move(D3, H3)),
+      Rook.moves(D3));
+  }
+
+  private <T> void assertSetEquals(Set<T> expected, Set<T> actual) {
+    assertEquals(expected.size(), actual.size());
+    for (var item : actual) {
+      assertTrue("Should contain: " + item, expected.contains(item));
+    }
   }
 }
