c33cd1b1f46e0360d971e373b479cff376868a30
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2020-02-09 10:00:40 +0100

    Refactoring.

--- src/main/java/cz/dusanrychnovsky/chessendgames/Range.java
+++ src/main/java/cz/dusanrychnovsky/chessendgames/Range.java
@@ -61,6 +61,7 @@ public class Range<T extends Comparable<T>> implements Iterable<T> {
     }
 
     static Axis<Position> get(Position from, Position to) {
+      // horizontal
       if (from.row() == to.row()) {
         if (from.column().compareTo(to.column()) <= 0) {
           // left to right
@@ -71,6 +72,7 @@ public class Range<T extends Comparable<T>> implements Iterable<T> {
           return pos -> pos.column().prev().map(col -> Position.get(col, pos.row()));
         }
       }
+      // vertical
       if (from.column() == to.column()) {
         if (from.row().compareTo(to.row()) <= 0) {
           // bottom to top
@@ -81,13 +83,14 @@ public class Range<T extends Comparable<T>> implements Iterable<T> {
           return pos -> pos.row().prev().map(row -> Position.get(pos.column(), row));
         }
       }
+      // diagonal
       if (from.column().distanceTo(to.column()) == from.row().distanceTo(to.row())) {
         if (from.column().compareTo(to.column()) <= 0) {
           if (from.row().compareTo(to.row()) <= 0) {
             // bottom left to top right
             return pos -> pos.row().next().flatMap(row -> pos.column().next().map(col -> Position.get(col, row)));
           }
-          if (from.row().compareTo(to.row()) > 0) {
+          else {
             // top left to bottom right
             return pos -> pos.row().prev().flatMap(row -> pos.column().next().map(col -> Position.get(col, row)));
           }
@@ -97,7 +100,7 @@ public class Range<T extends Comparable<T>> implements Iterable<T> {
             // bottom right to top left
             return pos -> pos.row().next().flatMap(row -> pos.column().prev().map(col -> Position.get(col, row)));
           }
-          if (from.row().compareTo(to.row()) > 0) {
+          else {
             // top right to bottom left
             return pos -> pos.row().prev().flatMap(row -> pos.column().prev().map(col -> Position.get(col, row)));
           }
