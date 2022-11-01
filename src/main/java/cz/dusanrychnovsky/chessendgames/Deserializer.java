c184f7fd2cf53e185554900e819c740747359f65
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2022-11-01 12:54:02 +0100

    Deserializer

---
+++ src/main/java/cz/dusanrychnovsky/chessendgames/Deserializer.java
@@ -0,0 +1,101 @@
+package cz.dusanrychnovsky.chessendgames;
+
+import cz.dusanrychnovsky.chessendgames.proto.Movesdb;
+
+import static cz.dusanrychnovsky.chessendgames.Color.*;
+import static cz.dusanrychnovsky.chessendgames.Column.*;
+import static cz.dusanrychnovsky.chessendgames.PieceType.*;
+import static cz.dusanrychnovsky.chessendgames.Row.*;
+
+public class Deserializer {
+
+  public Database fromProto(Color color, Movesdb.Database database) {
+    var builder = new Database.Builder();
+    for (var pair : database.getValuesList()) {
+      builder.add(
+        fromProto(color, pair.getSituation()),
+        fromProto(pair.getMove())
+      );
+    }
+    return builder.build();
+  }
+
+  public Situation fromProto(Color color, Movesdb.Situation situation) {
+    var builder = Board.builder();
+    for (var pair : situation.getValuesList()) {
+      builder.add(
+        fromProto(pair.getPiece()),
+        fromProto(pair.getPosition())
+      );
+    }
+    return new Situation(
+      color,
+      builder.build()
+    );
+  }
+
+  public Piece fromProto(Movesdb.Piece piece) {
+    return new Piece(
+      fromProto(piece.getColor()),
+      fromProto(piece.getType())
+    );
+  }
+
+  public PieceType fromProto(Movesdb.PieceType type) {
+    return switch (type) {
+      case King -> King;
+      case Rook -> Rook;
+      default -> throw new IllegalArgumentException(("Unknown piece type: " + type));
+    };
+  }
+
+  public Color fromProto(Movesdb.Color color) {
+    return switch (color) {
+      case White -> White;
+      case Black -> Black;
+      default -> throw new IllegalArgumentException("Unknown color: " + color);
+    };
+  }
+
+  public Move fromProto(Movesdb.Move move) {
+    return new Move(
+      fromProto(move.getFrom()),
+      fromProto(move.getTo())
+    );
+  }
+
+  public Position fromProto(Movesdb.Position position) {
+    return Position.get(
+      fromProto(position.getColumn()),
+      fromProto(position.getRow())
+    );
+  }
+
+  public Row fromProto(Movesdb.Row row) {
+    return switch (row) {
+      case R1 -> R1;
+      case R2 -> R2;
+      case R3 -> R3;
+      case R4 -> R4;
+      case R5 -> R5;
+      case R6 -> R6;
+      case R7 -> R7;
+      case R8 -> R8;
+      default -> throw new IllegalArgumentException("Unknown row: " + row);
+    };
+  }
+
+  public Column fromProto(Movesdb.Column column) {
+    return switch (column) {
+      case CA -> CA;
+      case CB -> CB;
+      case CC -> CC;
+      case CD -> CD;
+      case CE -> CE;
+      case CF -> CF;
+      case CG -> CG;
+      case CH -> CH;
+      default -> throw new IllegalArgumentException("Unknown column: " + column);
+    };
+  }
+}
---
+++ src/test/java/cz/dusanrychnovsky/chessendgames/DeserializerTest.java
@@ -0,0 +1,201 @@
+package cz.dusanrychnovsky.chessendgames;
+
+import cz.dusanrychnovsky.chessendgames.proto.Movesdb;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeserializerTest {
+
+  private static final Deserializer deserializer = new Deserializer();
+
+  // ==========================================================================
+  // from proto - row
+  // ==========================================================================
+
+  @Test
+  public void fromProto_convertsRow() {
+    assertEquals(Row.R3, deserializer.fromProto(Movesdb.Row.R3));
+  }
+
+  // ==========================================================================
+  // from proto - column
+  // ==========================================================================
+
+  @Test
+  public void fromProto_convertsColumn() {
+    assertEquals(Column.CE, deserializer.fromProto(Movesdb.Column.CE));
+  }
+
+  // ==========================================================================
+  // to proto - position
+  // ==========================================================================
+
+  @Test
+  public void toProto_convertsPosition() {
+    assertEquals(
+      Position.H3,
+      deserializer.fromProto(
+        Movesdb.Position.newBuilder()
+          .setColumn(Movesdb.Column.CH)
+          .setRow(Movesdb.Row.R3)
+          .build()));
+  }
+
+  // ==========================================================================
+  // to proto - move
+  // ==========================================================================
+
+  @Test
+  public void toProto_convertsMove() {
+    assertEquals(
+      new Move(Position.H3, Position.H1),
+      deserializer.fromProto(
+        Movesdb.Move.newBuilder()
+          .setFrom(
+            Movesdb.Position.newBuilder()
+              .setColumn(Movesdb.Column.CH)
+              .setRow(Movesdb.Row.R3))
+          .setTo(
+            Movesdb.Position.newBuilder()
+              .setColumn(Movesdb.Column.CH)
+              .setRow(Movesdb.Row.R1))
+          .build()));
+  }
+
+  // ==========================================================================
+  // from proto - color
+  // ==========================================================================
+
+  @Test
+  public void fromProto_convertsColor() {
+    assertEquals(Color.White, deserializer.fromProto(Movesdb.Color.White));
+  }
+
+  // ==========================================================================
+  // from proto - piece type
+  // ==========================================================================
+
+  @Test
+  public void fromProto_convertsPieceType() {
+    assertEquals(PieceType.King, deserializer.fromProto(Movesdb.PieceType.King));
+  }
+
+  // ==========================================================================
+  // to proto - piece
+  // ==========================================================================
+
+  @Test
+  public void toProto_convertsPiece() {
+    assertEquals(
+      Piece.BlackRook,
+      deserializer.fromProto(
+        Movesdb.Piece.newBuilder()
+          .setColor(Movesdb.Color.Black)
+          .setType(Movesdb.PieceType.Rook)
+          .build()));
+  }
+
+  // ==========================================================================
+  // to proto - situation
+  // ==========================================================================
+
+  @Test
+  public void toProto_convertsSituation() {
+    var result = deserializer.fromProto(
+      Color.White,
+      Movesdb.Situation.newBuilder()
+        .addValues(
+          Movesdb.Situation.Pair.newBuilder()
+            .setPiece(
+              Movesdb.Piece.newBuilder()
+                .setColor(Movesdb.Color.White)
+                .setType(Movesdb.PieceType.King))
+            .setPosition(
+              Movesdb.Position.newBuilder()
+                .setColumn(Movesdb.Column.CA)
+                .setRow(Movesdb.Row.R6))
+            .build())
+        .addValues(
+          Movesdb.Situation.Pair.newBuilder()
+            .setPiece(
+              Movesdb.Piece.newBuilder()
+                .setColor(Movesdb.Color.Black)
+                .setType(Movesdb.PieceType.Rook))
+            .setPosition(
+              Movesdb.Position.newBuilder()
+                .setColumn(Movesdb.Column.CH)
+                .setRow(Movesdb.Row.R1))
+            .build()
+        )
+        .build()
+    );
+    assertEquals(Color.White, result.color());
+    var pieces = result.board().pieces();
+    assertEquals(2, pieces.size());
+    assertEquals(Piece.WhiteKing, pieces.get(Position.A6));
+    assertEquals(Piece.BlackRook, pieces.get(Position.H1));
+  }
+
+  // ==========================================================================
+  // to proto - database
+  // ==========================================================================
+
+  @Test
+  public void toProto_convertsDatabase_singleEntry() {
+    var result = deserializer.fromProto(
+      Color.White,
+      Movesdb.Database.newBuilder()
+        .addValues(
+          Movesdb.Database.Pair.newBuilder()
+            .setSituation(
+              Movesdb.Situation.newBuilder()
+                .addValues(
+                  Movesdb.Situation.Pair.newBuilder()
+                    .setPiece(
+                      Movesdb.Piece.newBuilder()
+                        .setColor(Movesdb.Color.White)
+                        .setType(Movesdb.PieceType.King))
+                    .setPosition(
+                      Movesdb.Position.newBuilder()
+                        .setColumn(Movesdb.Column.CA)
+                        .setRow(Movesdb.Row.R6))
+                    .build())
+                .addValues(
+                  Movesdb.Situation.Pair.newBuilder()
+                    .setPiece(
+                      Movesdb.Piece.newBuilder()
+                        .setColor(Movesdb.Color.Black)
+                        .setType(Movesdb.PieceType.Rook))
+                    .setPosition(
+                      Movesdb.Position.newBuilder()
+                        .setColumn(Movesdb.Column.CH)
+                        .setRow(Movesdb.Row.R1))
+                    .build()))
+            .setMove(
+              Movesdb.Move.newBuilder()
+                .setFrom(
+                  Movesdb.Position.newBuilder()
+                    .setColumn(Movesdb.Column.CA)
+                    .setRow(Movesdb.Row.R6))
+                .setTo(
+                  Movesdb.Position.newBuilder()
+                    .setColumn(Movesdb.Column.CA)
+                    .setRow(Movesdb.Row.R7))))
+        .build()
+    );
+
+    var moves = result.moves();
+    assertEquals(1, moves.size());
+    assertEquals(
+      new Move(Position.A6, Position.A7),
+      moves.get(
+        new Situation(
+          Color.White,
+          Board.builder()
+            .add(Piece.WhiteKing, Position.A6)
+            .add(Piece.BlackRook, Position.H1)
+            .build())
+          ));
+  }
+}
